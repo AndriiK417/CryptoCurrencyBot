56065a9473d45fc7a3720742cdd5c1404141e297
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-05 10:30:44 +0300

    Update inline_buttons_handler.py
    To match the latest version

--- inline_buttons_handler.py
+++ inline_buttons_handler.py
@@ -1,26 +1,12 @@
 import telebot
 import requests
-from telebot import types
+from markups.currency_markup import currency_markup
+from markups.price_changes_markup import price_changes_markup
+from period_changes_handler import period
+from commands_handler import skip_price, skip_currency
 
 API_TOKEN = '6388083417:AAFnoBZpLQkrrF95Bj9uq0nYma5EUt9qs1k'
 bot = telebot.TeleBot(API_TOKEN)
-
-skip_currency = 0
-skip_price = 0
-
-currency_markup = types.InlineKeyboardMarkup()
-currency_markup.row_width = 2
-currency_markup.add(types.InlineKeyboardButton("<", callback_data="previous_currency"),
-types.InlineKeyboardButton(">", callback_data="next_currency"))
-
-price_changes_markup = types.InlineKeyboardMarkup()
-price_changes_markup.row_width = 2
-price_changes_markup.add(types.InlineKeyboardButton("<", callback_data="previous_price_changes"),
-types.InlineKeyboardButton(">", callback_data="next_price_changes"))
-
-charts_markup = types.InlineKeyboardMarkup()
-charts_markup.row_width = 6
-charts_markup.add(types.InlineKeyboardButton("Choose the other chart", callback_data="chart"))
 
 def handle_previous_currency_button(call):
     global skip_currency
@@ -66,7 +52,7 @@ def handle_previous_price_button(call):
     response_message = ''
 
     for i in range(10):
-        response_message += response[i]['name'] + ": " + str(response[i]['priceChange1w']) + '% \n'
+        response_message += response[i]['name'] + ": " + str(response[i][period]) + '% \n'
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=response_message, reply_markup=price_changes_markup)
 
@@ -82,6 +68,6 @@ def handle_next_price_button(call):
     response_message = ''
 
     for i in range(10):
-        response_message += response[i]['name'] + ": " + str(response[i]['priceChange1w']) + '% \n'
+        response_message += response[i]['name'] + ": " + str(response[i][period]) + '% \n'
 
     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=response_message, reply_markup=price_changes_markup)
