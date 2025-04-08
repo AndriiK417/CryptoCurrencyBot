4b9a47797eee41be1fb83e763a4140186d1df436
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-08 16:33:35 +0300

    charts_buttons_handler.py: update
    Added ability to choose chart period (1 day, 1 month, 3 months, 1 year)

--- charts_buttons_handler.py
+++ charts_buttons_handler.py
@@ -1,14 +1,15 @@
 import telebot
-import requests
+from markups.chart_period_markup import get_chart_period_markup
 
 API_TOKEN = '6388083417:AAFnoBZpLQkrrF95Bj9uq0nYma5EUt9qs1k'
 bot = telebot.TeleBot(API_TOKEN)
 
-
 def charts_buttons_handler(call):
+    # call.data — це, наприклад, 'BTCUSD'
     coin = call.data
-    CHARTS_API_LINK = f'https://api.chart-img.com/v1/tradingview/mini-chart?key=qJX6lruQMB9Yhkj7ub87z3vrFa8z6hI13AgoaLdS&symbol=BINANCE:{coin}&width=600&height=400&interval=3M&theme=light'
-
-    response = requests.get(CHARTS_API_LINK)
-    bot.send_photo(call.message.chat.id, response.content)
-    bot.send_message(call.message.chat.id, "Here's your chart")
+    # просимо обрати період
+    bot.send_message(
+        call.message.chat.id,
+        f"Оберіть період для графіка {coin}:",
+        reply_markup=get_chart_period_markup(coin)
+    )
