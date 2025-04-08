e0865ca4563dee3c43ceb179adabcb78118f78e6
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-08 16:50:59 +0300

    chart_period_markup.py: Fix
    Changed "1 hour", "1 day", "1 week" to "1 day", "1 month", "3 months", "1 year"

--- markups/chart_period_markup.py
+++ markups/chart_period_markup.py
@@ -2,13 +2,14 @@ from telebot import types
 
 def get_chart_period_markup(coin: str) -> types.InlineKeyboardMarkup:
     """
-    Повертає InlineKeyboardMarkup з кнопками для вибору періоду графіка.
-    call_data буде у форматі 'chart_<COIN>_<PERIOD>'.
+    Повертає InlineKeyboardMarkup з кнопками для вибору періоду графіка:
+    1D, 1M, 3M, 1Y
     """
-    markup = types.InlineKeyboardMarkup(row_width=3)
+    markup = types.InlineKeyboardMarkup(row_width=2)
     markup.add(
-        types.InlineKeyboardButton('1 год',  callback_data=f'chart_{coin}_1h'),
-        types.InlineKeyboardButton('1 день', callback_data=f'chart_{coin}_1d'),
-        types.InlineKeyboardButton('1 тиж',  callback_data=f'chart_{coin}_1w'),
+        types.InlineKeyboardButton('1 день',   callback_data=f'chart_{coin}_1D'),
+        types.InlineKeyboardButton('1 місяць', callback_data=f'chart_{coin}_1M'),
+        types.InlineKeyboardButton('3 місяці', callback_data=f'chart_{coin}_3M'),
+        types.InlineKeyboardButton('1 рік',     callback_data=f'chart_{coin}_1Y'),
     )
     return markup
