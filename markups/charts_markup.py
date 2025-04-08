34a8a0547a59f2d77ae120b5fd1ed52973c3422b
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-08 16:42:16 +0300

    charts_markup.py: update
    Added "selectchart_" prefix to crypto code to avoid confusion between callbacks

--- markups/charts_markup.py
+++ markups/charts_markup.py
@@ -1,15 +1,19 @@
 from telebot import types
 
 charts_markup = types.InlineKeyboardMarkup()
-variant1 = types.InlineKeyboardButton('Bitcoin', callback_data='BTCUSD')
-variant2 = types.InlineKeyboardButton('Ethereum', callback_data='ETHUSD')
-variant3 = types.InlineKeyboardButton('Tether', callback_data='APTUSDT') #?
-variant4 = types.InlineKeyboardButton('BNB', callback_data='BNBUSD') 
-variant5 = types.InlineKeyboardButton('XRP', callback_data='XRPUSD') #?
-variant6 = types.InlineKeyboardButton('USD Coin', callback_data='USDCUSDT') #?
-variant7 = types.InlineKeyboardButton('Lido Stacked Ether', callback_data="LDOUSD")
-variant8 = types.InlineKeyboardButton('Dogecoin', callback_data="DOGEUSD")
-variant9 = types.InlineKeyboardButton('Cardano', callback_data="ADAUSD")
-variant10 = types.InlineKeyboardButton('Solana', callback_data="SOLUSD")
+# тепер callback_data починається з selectchart_
+variant1  = types.InlineKeyboardButton('Bitcoin',               callback_data='selectchart_BTCUSD')
+variant2  = types.InlineKeyboardButton('Ethereum',              callback_data='selectchart_ETHUSD')
+variant3  = types.InlineKeyboardButton('Tether',                callback_data='selectchart_APTUSDT')
+variant4  = types.InlineKeyboardButton('BNB',                   callback_data='selectchart_BNBUSD')
+variant5  = types.InlineKeyboardButton('XRP',                   callback_data='selectchart_XRPUSD')
+variant6  = types.InlineKeyboardButton('USD Coin',              callback_data='selectchart_USDCUSDT')
+variant7  = types.InlineKeyboardButton('Lido Stacked Ether',    callback_data='selectchart_LDOUSD')
+variant8  = types.InlineKeyboardButton('Dogecoin',              callback_data='selectchart_DOGEUSD')
+variant9  = types.InlineKeyboardButton('Cardano',               callback_data='selectchart_ADAUSD')
+variant10 = types.InlineKeyboardButton('Solana',                callback_data='selectchart_SOLUSD')
 
-charts_markup.add(variant1, variant2, variant3, variant4, variant5, variant6, variant7, variant8, variant9, variant10)
+charts_markup.add(
+    variant1, variant2, variant3, variant4, variant5,
+    variant6, variant7, variant8, variant9, variant10
+)
