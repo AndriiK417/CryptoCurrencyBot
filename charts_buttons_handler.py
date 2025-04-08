ef7763eae329afcf7c4ecab394e1938ee98af05e
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-08 16:46:27 +0300

    charts_buttons_handler.py: Update
    Added code row that removes prefix from crypto code to be able to use it in charts generation

--- charts_buttons_handler.py
+++ charts_buttons_handler.py
@@ -5,9 +5,9 @@ API_TOKEN = '6388083417:AAFnoBZpLQkrrF95Bj9uq0nYma5EUt9qs1k'
 bot = telebot.TeleBot(API_TOKEN)
 
 def charts_buttons_handler(call):
-    # call.data — це, наприклад, 'BTCUSD'
-    coin = call.data
-    # просимо обрати період
+    # call.data — наприклад, 'selectchart_BTCUSD'
+    # видаляємо префікс, щоб отримати символ
+    _, coin = call.data.split('_', 1)
     bot.send_message(
         call.message.chat.id,
         f"Оберіть період для графіка {coin}:",
