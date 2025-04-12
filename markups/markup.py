eea07cbcb5048af6bcc5d2d020d69fdff42e9785
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-12 14:08:44 +0300

    markup.py: Update
    New button added: "Alerts"

--- markups/markup.py
+++ markups/markup.py
@@ -4,5 +4,6 @@ markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button1 = types.KeyboardButton('Currency')
 button2 = types.KeyboardButton('Price changes')
 button3 = types.KeyboardButton('Charts')
+button4 = types.KeyboardButton('Alerts')
 
-markup.add(button1, button2, button3)
+markup.add(button1, button2, button3, button4)
