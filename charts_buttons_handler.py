f29c8fef3c494978d9c4a2321f6f54122950a71a
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-19 16:39:30 +0300

    charts_buttons_handler.py: Update
    Imports updated to match updated charts_markup.py file

--- charts_buttons_handler.py
+++ charts_buttons_handler.py
@@ -1,5 +1,5 @@
 import telebot
-from markups.chart_period_markup import get_chart_period_markup
+from markups.charts_markup import charts_markup, get_chart_period_markup
 
 API_TOKEN = '6388083417:AAFnoBZpLQkrrF95Bj9uq0nYma5EUt9qs1k'
 bot = telebot.TeleBot(API_TOKEN)
