a575b6834c21d00bdc61a77b9a0753db76ab4830
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-20 09:49:47 +0300

    price_changes_markup.py: Update
    "Back" button markup added ("Price changes")

--- markups/price_changes_markup.py
+++ markups/price_changes_markup.py
@@ -2,5 +2,8 @@ from telebot import types
 
 price_changes_markup = types.InlineKeyboardMarkup()
 price_changes_markup.row_width = 2
-price_changes_markup.add(types.InlineKeyboardButton("<", callback_data="previous_price_changes"),
-types.InlineKeyboardButton(">", callback_data="next_price_changes"))
+price_changes_markup.add(
+    types.InlineKeyboardButton("<", callback_data="previous_price_changes"),
+    types.InlineKeyboardButton(">", callback_data="next_price_changes")
+)
+price_changes_markup.add(types.InlineKeyboardButton('« Назад', callback_data='price_back_to_period'))
