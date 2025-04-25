cc5367452abdd8b938ec39134b14cfe10eb01ec4
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-25 14:17:58 +0300

    markup.py: Update
    New button added: "Coins" (Shows chart and info for one chosen coin)

--- markups/markup.py
+++ markups/markup.py
@@ -1,9 +1,10 @@
 from telebot import types
 
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button0 = types.KeyboardButton('Монети')
 button1 = types.KeyboardButton('Currency')
 button2 = types.KeyboardButton('Price changes')
 button3 = types.KeyboardButton('Charts')
 button4 = types.KeyboardButton('Alerts')
 
-markup.add(button1, button2, button3, button4)
+markup.add(button0, button1, button2, button3, button4)
