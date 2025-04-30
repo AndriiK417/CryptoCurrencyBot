10bbcccc5aff4f9ae9c0381d5287c6f5b803d77b
AndriiK417 <59733278+AndriiK417@users.noreply.github.com>
2025-04-30 17:03:45 +0300

    Update markup.py
    Main feature's buttons were translated into Ukrainian

--- markups/markup.py
+++ markups/markup.py
@@ -2,9 +2,9 @@ from telebot import types
 
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 button0 = types.KeyboardButton('Монети')
-button1 = types.KeyboardButton('Currency')
-button2 = types.KeyboardButton('Price changes')
-button3 = types.KeyboardButton('Charts')
-button4 = types.KeyboardButton('Alerts')
+button1 = types.KeyboardButton('Ціни')
+button2 = types.KeyboardButton('Зміни цін')
+button3 = types.KeyboardButton('Графіки')
+button4 = types.KeyboardButton('Сповіщення')
 
 markup.add(button0, button1, button2, button3, button4)
